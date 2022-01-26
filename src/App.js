cb1eb04511370541dcda065cf2cd8bdf06ca7860
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-01-25 22:59:48 -0300

    1. Crie um componente chamado Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return <Header />;
   }
 }
 
