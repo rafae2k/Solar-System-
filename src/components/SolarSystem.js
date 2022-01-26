dbb20b6d8ac327154e9d363b53a1eb60ce1be6f4
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-01-26 09:59:00 -0300

    4. Renderize o componente Title dentro do componente SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
-const SolarSystem = () => <div data-testid="solar-system" />;
+const SolarSystem = () => (
+  <div>
+    <div data-testid="solar-system" />
+    <Title headline="Planetas" />
+  </div>
+);
 
 export default SolarSystem;
